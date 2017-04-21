661ed1813220bc03c821137187c79078ae9499cf
Adam Paul <ajpaul89@gmail.com>
2017-04-21 02:34:53 -0400

    Join service working. Just need pics/styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,9 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './navbar/navbar.component';
-import { AppServices } from './shared/app.service';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-  data: Object;
-
-    constructor(private appServices:AppServices, private http:Http) {
-
-      this.appServices.getJoin()
-          .subscribe(
-          (res) => {
-              this.data = res;
-          }
-      );
-  
-
-
-}
-
-
-}
+export class AppComponent {}
--- src/app/movies/movie-detail/movie-detail.component.ts
+++ src/app/movies/movie-detail/movie-detail.component.ts
@@ -10,10 +10,20 @@ import { ActivatedRoute } from '@angular/router';
 
 export class MovieDetailComponent implements OnInit {
 
-    movie;
+    movie: Object;
+    joinedData: String;
+    joinedDataArray: String[];
 
     constructor(private appServices:AppServices, private route:ActivatedRoute){
-
+        
+        //get the join file
+        this.appServices.getJoin()
+            .subscribe(
+            (res) => {
+                this.joinedData = res;
+                this.getActorsInMovie();
+            }
+        );
     }
 
     ngOnInit() {
@@ -22,8 +32,31 @@ export class MovieDetailComponent implements OnInit {
            .subscribe(
                 (res) => {
                     this.movie = this.appServices.convertMovieToObject(res);
+                    this.findActorsInCurrentMovie(this.movie);
                 }
         );
+
+        
+    }
+
+    //get all of the actors/movies combos and parse them/put them in an array
+    getActorsInMovie() {
+
+        if(!this.joinedData) return;
+
+        this.joinedDataArray = this.joinedData.split('\n');
+    }
+
+    findActorsInCurrentMovie(movie) {
+
+        let featuredActors: String[] = [];
+
+        for(let row of this.joinedDataArray){
+            if(row.includes(movie.id))
+                featuredActors.push(row.split('|')[0]);
+        }
+
+        console.log(featuredActors);
     }
 
 }
